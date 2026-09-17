ce9caaad4164557fdfc8f2bf9f36c865069b8169
Smirk22 <gregonasurada22@gmail.com>
2026-09-17 19:02:46 +0800

    removed some unused features

--- routes/web.php
+++ routes/web.php
@@ -4,18 +4,16 @@ use Illuminate\Support\Facades\Route;
 
 Route::inertia('/', 'welcome')->name('home');
 
-Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
-
-Route::post('/login', [AuthController::class, 'login']);
+Route::post('/login', [AuthController::class, 'login'])->name('login.store');
 
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
 Route::get('/register', [AuthController::class, 'showRegisterForm'])->name('register');
 
-Route::post('/register', [AuthController::class, 'register']);
+Route::post('/register', [AuthController::class, 'register'])->name('register.store');
 
 Route::middleware(['auth', 'verified'])->group(function () {
-   Route::inertia('rdashboard', 'rdashboard')->name('rdashboard');
+   Route::inertia('rdashboard', 'rdashboard')->name('rdashboard');php artisan route:clear
 });
 
 require __DIR__.'/settings.php';
