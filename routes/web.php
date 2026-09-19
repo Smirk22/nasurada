ac4223d5ccb87f5368ab02acf090556f267dfdae
Smirk22 <gregonasurada22@gmail.com>
2026-09-19 14:56:49 +0800

    slight changes to web.php

--- routes/web.php
+++ routes/web.php
@@ -8,11 +8,11 @@ use Illuminate\Support\Facades\Route;
 Route::inertia('/', 'welcome')->name('home');
 
 Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
-Route::post('/login', [AuthController::class, 'login']);
+Route::post('/login', [AuthController::class, 'login'])->name('login.store');
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
 Route::get('/register', [AuthController::class, 'showRegisterForm'])->name('register');
-Route::post('/register', [AuthController::class, 'register']);
+Route::post('/register', [AuthController::class, 'register'])->name('register.store');
 
 // Admin Routes
 Route::get('/adashboard', [AdminController::class, 'dashboard'])->name('adashboard');
